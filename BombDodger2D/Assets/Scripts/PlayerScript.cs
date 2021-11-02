ee2967a3cf1fd7ff6c6dfc3438f14ea31935ba79
YaziciUtku <utkuyazicirc@gmail.com>
2021-11-02 09:01:13 +0300

    02.11.2021
    PlayerMovement Added.

---
+++ BombDodger2D/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float playerHorizontalSpeed;
+
+    void Update()
+    {
+        MovePlayer(); 
+    }
+
+    private void MovePlayer()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        Vector2 currentPosition = transform.position;
+
+        if (h > 0)
+        {
+            currentPosition.x += playerHorizontalSpeed * Time.deltaTime; // Move Right
+        }
+        else if(h<0)
+        {
+            currentPosition.x -= playerHorizontalSpeed * Time.deltaTime; // Move Left
+        }
+
+        transform.position = currentPosition;
+    }
+}
